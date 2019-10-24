c60d4590002400bfeedb40844166221f5ec3b92a
Mauricio <mauricioferreyra548@gmail.com>
2019-10-24 15:45:08 -0300

    Detalle de producto semi finalizado

--- feriacultiva/feriacultiva/urls.py
+++ feriacultiva/feriacultiva/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('administrador/', views.Administrador),
@@ -40,4 +43,4 @@ urlpatterns = [
     #LOGIN and LOGOUT
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
