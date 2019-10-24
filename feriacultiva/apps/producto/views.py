ca8f81b028c09adda6b0416f7b84e03c7a2e8632
Mauricio <mauricioferreyra548@gmail.com>
2019-10-24 16:18:13 -0300

    Sugerencia de producto listo

--- feriacultiva/apps/producto/views.py
+++ feriacultiva/apps/producto/views.py
@@ -56,30 +56,9 @@ class DetalleProducto(DetailView):
     model = Producto
     template_name = 'producto/detalle_producto.html'
 
-
-# class BookListView(ListView):
-#     model=Book
-#     paginate_by=10
-#     template_name='book/book_list.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         print (context)
-#         paginator = context['paginator']
-#         page_numbers_range = 10  # Display 5 page numbers
-#         max_index = len(paginator.page_range)
-
-#         page = self.request.GET.get('page')
-#         print (self.request)
-#         current_page = int(page) if page else 1
-
-#         start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
-#         end_index = start_index + page_numbers_range
-#         if end_index >= max_index:
-#             end_index = max_index
-
-#         page_range = paginator.page_range[start_index:end_index]
-#         context['page_range'] = page_range
-#         return context
-
-# book_list = BookListView.as_view()
+    def get_context_data(self, **kwargs):
+        context = super(DetalleProducto, self).get_context_data(**kwargs)
+        context_object_name = 'Sugerencia'
+        list_sugerencia = Producto.objects.filter(categoria=self.kwargs['pk'])
+        context['Sugerencia'] = list_sugerencia
+        return context
