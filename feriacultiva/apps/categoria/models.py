b1c4885203c63a8d70bcf406c2a10711bfffe785
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-10-29 01:57:59 -0300

    foto cate

--- feriacultiva/apps/categoria/models.py
+++ feriacultiva/apps/categoria/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
 	nombre = models.CharField(max_length=30)
+	foto = models.ImageField(upload_to = 'categoria')
 
 	def __str__(self):
 		return self.nombre
