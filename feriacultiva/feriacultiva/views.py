f2f90ee1f1e830f0c7aec1aafbe4722438a4ea10
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-11-02 23:41:25 -0300

    arreglo

--- feriacultiva/feriacultiva/views.py
+++ feriacultiva/feriacultiva/views.py
@@ -3,10 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def Inicio(request):
-	if request.user.username == 'admin':
-		return render(request, 'administrador.html')	
-	else:
-		return render(request,'start.html')
+	return render(request,'start.html')
 
 @login_required
 def  Administrador(request):
