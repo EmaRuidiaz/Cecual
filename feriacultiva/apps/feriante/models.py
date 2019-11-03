db1111e491c2d7d46f1be574511215a91951e0ef
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-11-02 23:51:19 -0300

    arreglo

--- feriacultiva/apps/feriante/models.py
+++ feriacultiva/apps/feriante/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from apps.user.models import User
 # Create your models here.
 class Feriante(models.Model):
-	descripcion = models.TextField()
-	foto_feriante = models.ImageField(upload_to = 'feriante', null = True)
+	descripcion = models.TextField(max_length=500)
+	foto_feriante = models.ImageField(upload_to = 'feriante')
 	delivery = models.BooleanField(default=False, blank=True, null=True)
 	encargado = models.OneToOneField(User, on_delete=models.CASCADE)
 
