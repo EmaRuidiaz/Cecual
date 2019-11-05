44be93c95f9c7fa02687cf014e10f7dffe2d884b
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-11-04 22:48:37 -0300

    pedido

---
+++ feriacultiva/apps/pedido/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from django import forms
+from apps.pedido.models import Pedido
+    
+class PedidoForm(ModelForm):  
+
+    class Meta:
+        model = Pedido
+        fields = ['producto','cantidad','envio']
--- feriacultiva/apps/pedido/models.py
+++ feriacultiva/apps/pedido/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from apps.producto.models import Producto
+from apps.user.models import User
 
 # Create your models here.
+class Pedido(models.Model):
+	producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+	cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+	cantidad = models.IntegerField()
+	envio = models.BooleanField(default=False)
