c643f9dd2d938749ef90df8b3dc21ac2a792fb21
Mauricio <mauricioferreyra548@gmail.com>
2019-11-27 22:11:03 -0300

    Foto de Feriante arreglado

--- feriacultiva/apps/feriante/models.py
+++ feriacultiva/apps/feriante/models.py
@@ -3,13 +3,12 @@ from apps.user.models import User
 # Create your models here.
 class Feriante(models.Model):
 	descripcion = models.TextField(max_length=500)
-	foto_feriante = models.ImageField(upload_to = 'feriante')
+	foto_feriante = models.ImageField(upload_to = 'feriante', default='cliente/user.png')
 	delivery = models.BooleanField(default=False, blank=True, null=True)
 	encargado = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 	def __str__(self):
 		return self.encargado.first_name
-
 
 
